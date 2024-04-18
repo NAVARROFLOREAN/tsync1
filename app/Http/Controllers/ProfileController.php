2e333186ffd54b3f624612b6ffb428045ba61310
HazeNoquera <noquera.hazelann.bscs2021@gmail.com>
2024-04-19 03:28:58 +0800

    paayos nalang po ng design thank you:)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use Chatify\Facades\ChatifyMessenger as Chatify;
+use Illuminate\Support\Str;
 
 class ProfileController extends Controller
 {
@@ -28,10 +31,40 @@ class ProfileController extends Controller
     {
         $request->user()->fill($request->validated());
 
+        if ($request->hasFile('avatar')) {
+            // allowed extensions
+            $allowed_images = Chatify::getAllowedImages();
+
+            $file = $request->file('avatar');
+            // check file size
+            if ($file->getSize() < Chatify::getMaxUploadSize()) {
+                if (in_array(strtolower($file->extension()), $allowed_images)) {
+                    // delete the older one
+                    if (Auth::user()->avatar != config('chatify.user_avatar.default')) {
+                        $path = Chatify::getUserAvatarUrl(Auth::user()->avatar);
+                        if (Chatify::storage()->exists($path)) {
+                            Chatify::storage()->delete($path);
+                        }
+                    }
+                    // upload
+                    $avatar = Str::uuid() . "." . $file->extension();
+                    $update = User::where('id', Auth::user()->id)->update(['avatar' => $avatar]);
+                    $file->storeAs(config('chatify.user_avatar.folder'), $avatar, config('chatify.storage_disk_name'));
+                    $success = $update ? 1 : 0;
+                } else {
+                    $msg = "File extension not allowed!";
+                    $error = 1;
+                }
+            } else {
+                $msg = "File size you are trying to upload is too large!";
+                $error = 1;
+            }
+        }
+
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
-
+    
         $request->user()->save();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
